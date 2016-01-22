5652ca06825e23a27606922140cecbf1cf8a1249
Daniel Janus <dj@danieljanus.pl>
2016-01-22 12:37:29 +0100

    Move to Morfologik PoliMorf and case-sensitive dictionary.

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject polelum "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojars.nathell/morfologik-stemming "1.5.1-cidict1.8.1"]])
+                 [org.carrot2/morfologik-polish "2.0.1"]
+                 [org.carrot2/morfologik-stemming "2.0.1"]])
--- src/polelum.clj
+++ src/polelum.clj
@@ -1,9 +1,17 @@
 (ns polelum
-  (:require [clojure.java.io :as io])
-  (:import (morfologik.stemming PolishStemmer WordData)))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string])
+  (:import (morfologik.stemming.polish PolishStemmer)
+           (morfologik.stemming WordData)))
 
 (def disamb
   (read-string (slurp (io/resource "disamb.edn"))))
+
+(defn all-caps? [word]
+  (every? #(or (Character/isUpperCase %) (not (Character/isLetter %))) word))
+
+(defn title-case [word]
+  (str (Character/toTitleCase (first word)) (string/lower-case (subs word 1))))
 
 (let [stemmer (PolishStemmer.)]
   (defn stem [word]
@@ -11,7 +19,21 @@
       (let [res (map (fn [^WordData x] {:base (-> x .getStem str) :tag (-> x .getTag str)}) (.lookup stemmer word))]
         (when-not (empty? res) res)))))
 
+(defn lemmatize-single [^String word]
+  (or (disamb word) (:base (first (stem word)))))
+
+(defn lemmatize-multiple [^String word]
+  (let [d (disamb word)]
+    (concat (when d [d]) (map :base (stem word)))))
+
 (defn lemmatize [^String word]
-  (or (disamb word)
-      (let [lc (.toLowerCase word)]
-        (or (disamb lc) (:base (first (stem lc)) lc)))))
+  (or (lemmatize-single word)
+      (lemmatize-single (.toLowerCase word))
+      (when (all-caps? word) (lemmatize-single (title-case word)))
+      word))
+
+(defn lemmatize-all [^String word]
+  (let [v (distinct (concat (lemmatize-multiple word)
+                            (lemmatize-multiple (.toLowerCase word))
+                            (when (all-caps? word) (lemmatize-multiple (title-case word)))))]
+    (if (seq v) (vec v) [word])))
