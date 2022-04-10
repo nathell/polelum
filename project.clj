06f97b6c2031ab5a8a0e10985fa341b1c8f3fca8
Daniel Janus <dj@danieljanus.pl>
2022-04-10 13:48:07 +0200

    Add deps.edn
    So that it can be used as a Git dependency (I’d rather not publish it
    on Clojars). I’m keeping project.clj around for now but will probably
    eventually remove it.
    
    Also bumps the Morfologik versions to latest.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject polelum "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.carrot2/morfologik-polish "2.0.1"]
-                 [org.carrot2/morfologik-stemming "2.0.1"]])
+                 [org.carrot2/morfologik-polish "2.1.8"]
+                 [org.carrot2/morfologik-stemming "2.1.8"]])
